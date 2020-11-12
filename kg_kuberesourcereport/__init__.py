3365cacfe39cda573d700ad7ae6c0ed742889e47
Rangel Reale <rangelspam@gmail.com>
2020-11-12 08:16:19 -0300

    release 0.8.0

--- kg_kuberesourcereport/__init__.py
+++ kg_kuberesourcereport/__init__.py
@@ -7,7 +7,7 @@ from .option import (
     KubeResourceReportOptions_Default_Resources_DeploymentNGINX,
 )
 
-__version__ = "0.7.2"
+__version__ = "0.8.0"
 
 __all__ = [
     'KubeResourceReportBuilder',
